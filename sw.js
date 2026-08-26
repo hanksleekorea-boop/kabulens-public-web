8ca84768d1aa0f0575eb1feeaeecbd34f6452984
hanksleekorea-boop <hanksleekorea@gmail.com>
2026-08-26 16:58:38 +0700

    fix: preserve v0.29 to v0.30 PWA upgrades

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kabulens-shell-v30-2-public-beta1';
+const CACHE='kabulens-shell-v30-2-public-beta2';
 const SHELL=['./','./index.html','./styles.css','./storage.js','./pc_runtime.js','./mobile_runtime.js','./research_runtime.js','./catalog_10000.json','./app.js','./evidence_runtime.js','./billing_runtime.js','./license_runtime.js','./manifest.webmanifest','./icon.svg','./legal/terms.html','./legal/privacy.html','./status.html','./support.html','./robots.txt','./sitemap.xml'];
 const APP_SHELL=new URL('./index.html',self.registration.scope).href;
 const APP_SCOPE_PATH=new URL(self.registration.scope).pathname;
